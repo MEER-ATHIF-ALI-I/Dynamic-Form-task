48cc19517fc4f2b1d32a034feeafd3b2b54c07b5
MEER ATHIF ALI I <meerathifali@gmail.com>
2021-12-22 19:46:58 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-danger ">
+        <div className="container-fluid"></div>
+      </nav>
+
+      <nav className="navbar navbar-dark bg-danger ">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">Dynamic Form Task</span>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
